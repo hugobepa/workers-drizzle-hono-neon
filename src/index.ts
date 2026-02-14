6e9c78f99eb237e07b3f496d8e98d10028f0b360
hugobepa <hugo_bcn@hotmail.com>
2026-02-14 19:55:52 +0100

    commit cloudflare

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool, neon } from '@neondatabase/serverless';
+import { products } from './db/schema';
 import { Hono } from 'hono';
 
 export type Env = {
@@ -8,7 +11,10 @@ const app = new Hono<{ Bindings: Env }>();
 
 app.get('/', async (c) => {
 	c.env.DATABASE_URL;
-	return c.json({ message: 'Hello, World!' });
+	//const sql = neon(c.env.DATABASE_URL);
+	const db = drizzle(c.env.DATABASE_URL);
+	const allProducts = await db.select().from(products);
+	return c.json({ message: 'Hello, World!', products: allProducts });
 });
 
 export default app;
